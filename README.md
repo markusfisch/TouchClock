959fa85aef21fa9b94a61521c364d18ed954b0ec
Markus Fisch <mf@markusfisch.de>
2013-09-19 22:37:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Touch Clock
+===========
+
+A editable clock view for Android. Gives users a straight forward,
+traditional way of setting a point in time (and - optionally - a duration).
+
+Minnimum SDK version 4.
+
+![Screenshot](http://markusfisch.github.io/TouchClock/screenshot.png)
+
+See TouchClockSample for a sample implementation.
+
+Features
+--------
+
+TouchClockView has some public members for customization:
+
+* Show/hide duration by setting _useDuration_ to true/false
+* Set colors for
+	* face marks in _markColor_
+	* hour hand in _hourColor_
+	* minute hand in _minuteColor_
+	* duration hand in _stopColor_
+
+You can also subclass TouchClockView and overwrite _getClockFace()_ to
+draw your own custom clock face.
+
+License
+-------
+
+This code is licensed under the [MIT license][1].
+
+[1]: http://opensource.org/licenses/mit-license.php
---
+++ touchclock/src/de/markusfisch/android/library/touchclock/TouchClockView.java
@@ -0,0 +1,518 @@
+package de.markusfisch.android.library.touchclock;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.Calendar;
+
+public class TouchClockView extends View
+{
+	public interface OnTimeUpdateListener
+	{
+		public void onTimeUpdate( int hour, int minute, int duration );
+	}
+
+	static private enum HandType
+	{
+		NOHAND,
+		HOUR,
+		MINUTE,
+		STOP
+	}
+
+	private static final float DOUBLE_PI = (float)Math.PI*2f;
+	private static final float HALF_PI = (float)Math.PI/2f;
+	private static final float RAD_MINUTE = DOUBLE_PI/60f;
+	private static final float MINUTE_RAD = 60f/DOUBLE_PI;
+	private static final float HOUR_RAD = 12f/DOUBLE_PI;
+	private static final float RAD_TO_DEGREE = 180f/(float)Math.PI;
+	private static final float MPH_PER_RAD = 720f/DOUBLE_PI;
+	private static final float RAD_PER_MPH = DOUBLE_PI/720f;
+	private static final float RAD_PER_5MPH = DOUBLE_PI/144f;
+
+	public boolean useDuration = true;
+	public int markColor = 0xff0099cc;
+	public int hourColor = 0xff0099cc;
+	public int minuteColor = 0xff33b5e5;
+	public int stopColor = 0xffaa66cc;
+
+	protected Paint handPaint = new Paint( Paint.ANTI_ALIAS_FLAG );
+	protected Paint durationPaint = new Paint( Paint.ANTI_ALIAS_FLAG );
+	protected Hand hour = new Hand();
+	protected Hand minute = new Hand();
+	protected Hand stop = new Hand();
+
+	private final RectF stopRect = new RectF();
+	private HandType handType = HandType.NOHAND;
+	private Bitmap clockFace = null;
+	private float touchRadius;
+	private float centerX;
+	private float centerY;
+	private float hourLastAngle;
+	private float minuteLastAngle;
+	private int hourValue;
+	private int minuteValue;
+	private boolean am = true;
+	private OnTimeUpdateListener onTimeUpdateListener;
+
+	public TouchClockView( Context context )
+	{
+		super( context );
+		init();
+	}
+
+	public TouchClockView( Context context, AttributeSet attrs )
+	{
+		super( context, attrs );
+		init();
+	}
+
+	public TouchClockView( Context context, AttributeSet attrs, int defStyle )
+	{
+		super( context, attrs, defStyle );
+		init();
+	}
+
+	public void setOnTimeUpdateListener( OnTimeUpdateListener listener )
+	{
+		onTimeUpdateListener = listener;
+	}
+
+	public void setTime( int h, int m )
+	{
+		if( h > 12 )
+			am = false;
+
+		hourValue = h;
+		minuteValue = m;
+
+		setAm( hourValue );
+
+		hour.angle = getHourAngle( hourValue, minuteValue );
+		hourLastAngle = getClockAngle( hour.angle );
+		minute.angle = getMinuteAngle( minuteValue );
+		minuteLastAngle = getClockAngle( minute.angle );
+
+		update();
+	}
+
+	public void setDuration( int minutes )
+	{
+		int h = hourValue;
+		int m = minuteValue;
+
+		h += minutes/60;
+		m += minutes%60;
+
+		stop.angle = getHourAngle( h, m );
+
+		update();
+	}
+
+	public int getHour()
+	{
+		return hourValue;
+	}
+
+	public int getMinute()
+	{
+		return minuteValue;
+	}
+
+	public int getDuration()
+	{
+		return Math.round(
+			MPH_PER_RAD*
+			getAngleDifference( stop.angle-hour.angle ) );
+	}
+
+	@Override
+	public void onSizeChanged( int w, int h, int oldw, int oldh )
+	{
+		super.onSizeChanged( w, h, oldw, oldh );
+
+		final float radius = Math.min( w, h )*.5f;
+		final float dp = getContext()
+			.getResources()
+			.getDisplayMetrics()
+			.density;
+
+		final float cell = radius*.6f/3f;
+		touchRadius = dp*24f;
+		hour.length = radius*.4f;
+
+		if( cell < touchRadius*2f )
+		{
+			stop.length = hour.length+cell;
+			minute.length = stop.length+cell;
+		}
+		else
+		{
+			stop.length = radius*.6f;
+			minute.length = radius*.8f;
+		}
+
+		centerX = w/2;
+		centerY = h/2;
+
+		clockFace = getClockFace(
+			w,
+			h,
+			centerX,
+			centerY,
+			radius,
+			dp*2f,
+			markColor );
+
+		hour.color = hourColor;
+		minute.color = minuteColor;
+		stop.color = stopColor;
+
+		handPaint.setStyle( Paint.Style.STROKE );
+		handPaint.setStrokeWidth( dp*2f );
+
+		durationPaint.setStyle( Paint.Style.FILL );
+		durationPaint.setColor( 0x44000000 | (stop.color & 0xffffff) );
+
+		stopRect.left = centerX-stop.length;
+		stopRect.top = centerY-stop.length;
+		stopRect.right = centerX+stop.length;
+		stopRect.bottom = centerY+stop.length;
+	}
+
+	@Override
+	public void onDraw( Canvas c )
+	{
+		super.onDraw( c );
+
+		if( clockFace == null )
+			return;
+
+		c.drawBitmap( clockFace, 0, 0, null );
+
+		if( useDuration )
+		{
+			final float d = getAngleDifference(
+				stop.angle-hour.angle );
+
+			c.drawArc(
+				stopRect,
+				hour.angle*RAD_TO_DEGREE,
+				d*RAD_TO_DEGREE,
+				true,
+				durationPaint );
+		}
+
+		hour.draw( c );
+		minute.draw( c );
+
+		if( useDuration )
+			stop.draw( c );
+
+		handPaint.setStyle( Paint.Style.FILL );
+		handPaint.setColor( markColor );
+		c.drawCircle(
+			centerX,
+			centerY,
+			touchRadius*.3f,
+			handPaint );
+	}
+
+	@Override
+	public boolean onTouchEvent( MotionEvent motionEvent )
+	{
+		final float x = motionEvent.getX();
+		final float y = motionEvent.getY();
+		float angle = 0;
+		float duration = 0;
+
+		if( handType != HandType.NOHAND )
+		{
+			angle = (float)Math.atan2( y-centerY, x-centerX );
+			duration = getAngleDifference( stop.angle-hour.angle );
+		}
+
+		switch( (motionEvent.getAction() & MotionEvent.ACTION_MASK) )
+		{
+			case MotionEvent.ACTION_DOWN:
+				if( inRadius( x, y, hour.x, hour.y, touchRadius ) )
+					handType = HandType.HOUR;
+				else if( inRadius( x, y, minute.x, minute.y, touchRadius ) )
+					handType = HandType.MINUTE;
+				else if( inRadius( x, y, stop.x, stop.y, touchRadius ) )
+					handType = HandType.STOP;
+				break;
+			case MotionEvent.ACTION_MOVE:
+				switch( handType )
+				{
+					case HOUR:
+						hour.angle = angle;
+						stop.angle = getAngleDifference(
+							hour.angle+duration );
+
+						// determine if am or pm
+						{
+							final float c = getClockAngle( hour.angle );
+							float a = c-hourLastAngle;
+							final float r = Math.abs( a );
+
+							if( DOUBLE_PI-r < r )
+								a = -a;
+
+							if( (a > 0 && hourLastAngle > c) ||
+								(a < 0 && hourLastAngle < c) )
+								am ^= true;
+
+							hourLastAngle = c;
+						}
+
+						hourValue = (Math.round(
+								HOUR_RAD*
+								getClockAngle( hour.angle ) )+
+								(am ? 0 : 12)
+							)%24;
+
+						update();
+						break;
+					case MINUTE:
+						minute.angle = angle;
+
+						// determine if hour needs modification
+						{
+							final float c = getClockAngle( minute.angle );
+							float a = c-minuteLastAngle;
+							final float r = Math.abs( a );
+
+							if( DOUBLE_PI-r < r )
+								a = -a;
+
+							if( a > 0 && minuteLastAngle > c )
+								setAm( (hourValue = getWrappedHour(
+									++hourValue )) );
+							else if( a < 0 && minuteLastAngle < c )
+								setAm( (hourValue = getWrappedHour(
+									--hourValue )) );
+
+							minuteLastAngle = c;
+						}
+
+						minuteValue = (int)Math.floor(
+								MINUTE_RAD*
+								getClockAngle( minute.angle )
+							)%60;
+
+						hour.angle = getHourAngle(
+							hourValue,
+							minuteValue );
+						stop.angle = getAngleDifference(
+							hour.angle+duration );
+
+						update();
+						break;
+					case STOP:
+						stop.angle = Math.round(
+							angle/RAD_PER_5MPH )*RAD_PER_5MPH;
+
+						update();
+						break;
+				}
+				break;
+			case MotionEvent.ACTION_UP:
+				switch( handType )
+				{
+					case HOUR:
+					case MINUTE:
+						hour.angle = getHourAngle(
+							hourValue,
+							minuteValue );
+						minute.angle = getMinuteAngle(
+							minuteValue );
+						stop.angle = getAngleDifference(
+							hour.angle+duration );
+
+						update();
+						break;
+				}
+
+				handType = HandType.NOHAND;
+				break;
+		}
+
+		return true;
+	}
+
+	protected static Bitmap getClockFace(
+		int width,
+		int height,
+		float cx,
+		float cy,
+		float radius,
+		float strokeWidth,
+		int color )
+	{
+		final Bitmap bitmap;
+
+		java.lang.System.gc();
+
+		if( (bitmap = Bitmap.createBitmap(
+			width,
+			height,
+			Bitmap.Config.ARGB_8888 )) == null )
+			return null;
+
+		final Canvas c = new Canvas( bitmap );
+		final Paint p = new Paint( Paint.ANTI_ALIAS_FLAG );
+
+		p.setStyle( Paint.Style.STROKE );
+		p.setStrokeWidth( strokeWidth );
+		p.setColor( color );
+
+		float r1 = radius*.9f;
+		float r2 = radius*.95f;
+		float a = 0;
+
+		for( int n = 0;
+			a < DOUBLE_PI;
+			a += RAD_MINUTE, ++n )
+		{
+			final float cos = (float)Math.cos( a );
+			final float sin = (float)Math.sin( a );
+			final float r;
+
+			if( n % 5 > 0 )
+				r = r2;
+			else
+				r = r1;
+
+			c.drawLine(
+				cx+r*cos,
+				cy+r*sin,
+				cx+radius*cos,
+				cy+radius*sin,
+				p );
+		}
+
+		return bitmap;
+	}
+
+	protected void init()
+	{
+		Calendar c = Calendar.getInstance();
+
+		setTime(
+			c.get( Calendar.HOUR_OF_DAY ),
+			c.get( Calendar.MINUTE ) );
+
+		setDuration( 120 );
+	}
+
+	protected void update()
+	{
+		if( onTimeUpdateListener != null )
+			onTimeUpdateListener.onTimeUpdate(
+				hourValue,
+				minuteValue,
+				getDuration() );
+
+		invalidate();
+	}
+
+	private static boolean inRadius(
+		final float tx,
+		final float ty,
+		final float cx,
+		final float cy,
+		final float r )
+	{
+		final float dx = cx-tx;
+		final float dy = cy-ty;
+
+		return (Math.sqrt( dx*dx+dy*dy ) < r);
+	}
+
+	private void setAm( final int h )
+	{
+		am = (h <= 11);
+	}
+
+	private static float getHourAngle( final int h, final int m )
+	{
+		return -HALF_PI+RAD_PER_MPH*((h > 12 ? h-12 : h)*60+m);
+	}
+
+	private static float getMinuteAngle( final int m )
+	{
+		return -HALF_PI+RAD_MINUTE*m;
+	}
+
+	private static float getClockAngle( final float a )
+	{
+		return getAngleDifference( a+HALF_PI );
+	}
+
+	private static float getAngleDifference( float a )
+	{
+		while( a < 0 )
+			a += DOUBLE_PI;
+
+		while( a > DOUBLE_PI )
+			a -= DOUBLE_PI;
+
+		return a;
+	}
+
+	private static int getWrappedHour( int h )
+	{
+		while( h < 0 )
+			h += 24;
+
+		while( h > 24 )
+			h -= 24;
+
+		return h%24;
+	}
+
+	protected class Hand
+	{
+		public float x;
+		public float y;
+		public float angle;
+		public float length;
+		public int color;
+
+		public void draw( Canvas c )
+		{
+			x = centerX+length*(float)Math.cos( angle );
+			y = centerY+length*(float)Math.sin( angle );
+
+			handPaint.setStyle( Paint.Style.STROKE );
+			handPaint.setColor( color );
+			c.drawLine(
+				centerX,
+				centerY,
+				x,
+				y,
+				handPaint );
+
+			handPaint.setStyle( Paint.Style.FILL );
+			c.drawCircle(
+				x,
+				y,
+				touchRadius*.3f,
+				handPaint );
+
+			handPaint.setColor( 0x88000000 | (color & 0xffffff) );
+			c.drawCircle(
+				x,
+				y,
+				touchRadius,
+				handPaint );
+		}
+	}
+}
